597488f4c51d2d65e498fcb75b4e8539d71f07d7
geunwoobaek <39114544+geunwoobaek@users.noreply.github.com>
2021-04-14 01:49:04 +0900

    컨닝2
    컨닝2

--- week1/geunwoo/1014.cpp
+++ week1/geunwoo/1014.cpp
@@ -4,31 +4,34 @@ using namespace std;
 int testCase, N, M;
 int cache[11][1 << 10 + 1];
 int bitMap[11];
-int dfs(int cnt, int cur, int idx, int line, int bitcheck, vector<vector<char>> &Room)
+int dfs(int cnt, int cur, int idx, int line, int bitcheck, vector<string> &Room)
 {
-    if (idx >= M || line == N)
+    if (line == N || cur >= bitMap[M])
         return 0;
     int &result = cache[line][bitcheck];
-    if (result != -1)
+    if (result != -1&&cur==0)
         return result;
     for (int i = idx; i < M; i++)
     {
-        int next = cur + bitMap[i];
+        int next = cur | bitMap[i];
         if (Room[line][i] != 'x' && (i == M || (bitMap[i + 1] & bitcheck) == 0) && (i == 0 || (bitMap[i - 1] & bitcheck) == 0))
         {
             result = max(result, dfs(cnt + 1, next, i + 2, line, bitcheck, Room)); //
             result = max(result, cnt + 1 + dfs(0, 0, 0, line + 1, next, Room));
         }
     }
-    if(result==-1) return 0;
-    else return result;
+    result = max(result, dfs(0, 0, 0, line + 1, 0, Room));
+    if (result == -1)
+        return result = 0;
+    else
+        return result;
 }
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    for (int i = 0; i < 11; i++)
+    for (int i = 0; i <= 12; i++)
     {
         bitMap[i] = 1 << i;
     }
@@ -37,13 +40,12 @@ int main()
     {
         cin >> N >> M;
         memset(cache, -1, sizeof(cache));
-        vector<vector<char>> vec(N, vector<char>(M));
+        vector<string> vec(N);
         for (int i = 0; i < N; i++)
-            for (int j = 0; j < M; j++)
-                cin >> vec[i][j];
+            cin >> vec[i];
         int result = 0;
-        for (int i = 0; i < N; i++)
-            result = max(result, dfs(0, 0, 0, i, 0, vec));
+        // for (int i = 0; i < N; i++)
+        result = dfs(0, 0, 0, 0, 0, vec);
         cout << result << "\n";
     }
     return 0;
